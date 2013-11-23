c9c9c337efb054df8a44081a0c1b5ba0cf118810
Noah Ryan <nsmryan@gmail.com>
2013-11-23 16:23:39 -0500

    Wrote streamFile.hs to stream a given file to a given ip/port over UDP. If given a chunk size, it will only send packets of that size.

---
+++ streamFile.hs
@@ -0,0 +1,61 @@
+import Prelude as P
+
+import GHC.IO.Handle
+import System.IO as IO
+import System.Environment
+
+import Network.Socket
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+import qualified Control.Concurrent as C
+
+import Data.Conduit
+import Data.Conduit.Binary as CB
+import Data.Conduit.Network
+import Data.Conduit.Filesystem
+import Data.ByteString as BS
+import Data.Word
+
+
+oneSecond = 1000000 
+
+pipeFileHandle hIn pos chunkSize = do
+  size <- liftIO $ fromIntegral <$> hFileSize hIn
+  let bytes = size - pos
+  if (pos < size) && ((chunkSize <= 0) || (bytes >= chunkSize))
+    then do
+      let bytesToRead = if chunkSize <= 0 then bytes else chunkSize
+      input <- liftIO $ BS.hGet hIn bytesToRead
+      yield input
+      pipeFileHandle hIn (pos + bytesToRead) chunkSize
+    else do
+      liftIO $ C.threadDelay oneSecond
+      pipeFileHandle hIn pos chunkSize
+
+allocSocket ip port = do
+  socket <- socket AF_INET Datagram defaultProtocol
+  hostAddr <- inet_addr ip
+  connect socket $ SockAddrInet port hostAddr
+  return socket
+  
+
+main = do
+  args <- getArgs
+  let (fileName:ip:portStr:optional) = args
+  let chunkSize = if P.null optional then (-1) else read . P.head $ optional
+  mapM_ IO.putStrLn $
+    [
+      "fileName = " ++ fileName
+    , "ip = " ++ ip
+    , "port = " ++ portStr
+    ]
+  let port = fromIntegral $ (read portStr :: Int)
+  let openH = IO.openFile fileName IO.ReadMode
+  withSocketsDo $ runResourceT $ do
+    (sockKey, sock) <- allocate (allocSocket ip port) sClose
+    (hKey, h) <- allocate openH hClose
+    pipeFileHandle h 0 chunkSize $$ sinkSocket sock
